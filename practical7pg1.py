d132c0e72af4303045217636f3f118e2e858ac16
vidhiarbat07 <arbatva@jdcoem.ac.in>
2026-09-13 15:34:50 +0530

    Add files via upload

---
+++ practical7pg1.py
@@ -0,0 +1,13 @@
+#Program 1: Linear Search
+def linear_search(arr, key):
+ for i in range(len(arr)):
+ if arr[i] == key:
+ return i
+ return -1
+arr = [10, 25, 30, 45, 60]
+key = 30
+result = linear_search(arr, key)
+if result != -1:
+ print("Element found at index:", result)
+else:
+    print("Element not found")
---
+++ practical7pg2.py
@@ -0,0 +1,16 @@
+#🧪 Program 2: Binary Search
+def binary_search(arr, key):
+ low = 0
+ high = len(arr) - 1
+ while low <= high:
+ mid = (low + high) // 2
+ if arr[mid] == key:
+ return mid
+ elif arr[mid] < key:
+ low = mid + 1
+ else:
+ high = mid - 1
+ return -1
+arr = [10, 20, 30, 40, 50]
+key = 40
+print("Index:", binary_search(arr, key))
